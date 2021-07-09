87f5b8d0ca88ba072dc42ee5ad78a3263542861a
asiask97 <73076054+asiask97@users.noreply.github.com>
2021-07-09 20:48:05 +0100

    Update settings.py

--- flask_qa/settings.py
+++ flask_qa/settings.py
@@ -1,6 +1,10 @@
 
-import os 
+import os
+import re
 
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+# rest of connection code using the connection string `uri`
 SECRET_KEY = os.environ.get('SECRET_KEY')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
